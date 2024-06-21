e7e837109556718ee3d7ae1b68de98594c24958c
hemanthcse1 <hemanthcse1@gmail.com>
2024-06-21 21:31:50 +0530

    1. Adapter design pattern

---
+++ src/main/java/com/hemanth/designpatterns/structural/adapter/MediaAdapter.java
@@ -0,0 +1,22 @@
+package com.hemanth.designpatterns.structural.adapter;
+
+public class MediaAdapter implements MediaPlayer {
+    AdvancedMediaPlayer advancedMusicPlayer;
+
+    public MediaAdapter(String audioType) {
+        if (audioType.equalsIgnoreCase("vlc")) {
+            advancedMusicPlayer = new AdvancedMediaPlayer();
+        } else if (audioType.equalsIgnoreCase("mp4")) {
+            advancedMusicPlayer = new AdvancedMediaPlayer();
+        }
+    }
+
+    @Override
+    public void play(String audioType, String fileName) {
+        if (audioType.equalsIgnoreCase("vlc")) {
+            advancedMusicPlayer.playVlc(fileName);
+        } else if (audioType.equalsIgnoreCase("mp4")) {
+            advancedMusicPlayer.playMp4(fileName);
+        }
+    }
+}
---
+++ src/main/java/com/hemanth/designpatterns/structural/adapter/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.hemanth.designpatterns.structural.adapter;
+
+interface MediaPlayer {
+    void play(String audioType, String fileName);
+}
